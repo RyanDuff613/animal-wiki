4a72789b579d20f2d024b7f9616ad4bcdb8607e0
William Pratt <willpratt78@gmail.com>
2020-03-26 10:27:01 -0700

    change js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  var clicked=confirm()
+
+  if($('#snakes').show();) {
+    $('#snakeButton').click(function(){
+    $('#snakes').show();
+    $('#turtles').hide();
+    $('#dogs').hide();
+  }  
+
+});
